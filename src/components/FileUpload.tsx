23a59d4933150fabfdcf159ef037d88c4c9a4f4d
Sanjeeban Das <iamsanjeebandas@gmail.com>
2024-07-28 21:14:56 +0530

    created fileupload component and added aws s3

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,16 +1,29 @@
 "use client";
+import { uploadToS3 } from "@/lib/db/s3"; // Ensure the path is correct
 import { Inbox } from "lucide-react";
 import React from "react";
 import { useDropzone } from "react-dropzone";
 
-const FileUpload = () => {
+const FileUpload: React.FC = () => {
   const { getRootProps, getInputProps } = useDropzone({
     accept: { "application/pdf": [".pdf"] },
     maxFiles: 1,
-    onDrop: (acceptedFiles) => {
+    onDrop: async (acceptedFiles) => {
       console.log(acceptedFiles);
+      const file = acceptedFiles[0];
+      if (file.size > 10 * 1024 * 1024) {
+        alert("Please upload a smaller file");
+        return;
+      }
+      try {
+        const data = await uploadToS3(file);
+        console.log("Data", data);
+      } catch (error) {
+        console.error(error);
+      }
     },
   });
+
   return (
     <div className="p-2 bg-white rounded-xl">
       <div
---
+++ src/lib/db/s3.ts
@@ -0,0 +1,63 @@
+import AWS from "aws-sdk";
+
+export async function uploadToS3(file: File) {
+  try {
+    // Check if environment variables are set
+    if (
+      !process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID ||
+      !process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY_ID ||
+      !process.env.NEXT_PUBLIC_S3_BUCKET_NAME
+    ) {
+      throw new Error(
+        "AWS credentials or bucket name are not set in environment variables"
+      );
+    }
+
+    // Update AWS config
+    AWS.config.update({
+      accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID,
+      secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY_ID,
+      region: "ap-south-1", // Ensure region is set here
+    });
+
+    const s3 = new AWS.S3();
+
+    const file_key =
+      "uploads/" + Date.now().toString() + file.name.replace(" ", "-");
+
+    const params = {
+      Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME,
+      Key: file_key,
+      Body: file,
+    };
+
+    const upload = s3
+      .putObject(params)
+      .on("httpUploadProgress", (evt) => {
+        if (evt.total) {
+          console.log(
+            "Uploading to S3...",
+            Math.floor((evt.loaded * 100) / evt.total).toString() + "%"
+          );
+        }
+      })
+      .promise();
+
+    await upload.then((data) => {
+      console.log("Successfully uploaded to S3!", file_key);
+    });
+
+    return {
+      file_key,
+      file_name: file.name,
+    };
+  } catch (error) {
+    console.error("Error uploading to S3", error);
+    throw error; // Re-throw the error after logging it
+  }
+}
+
+export function getS3Url(file_key: string) {
+  const url = `https://${process.env.NEXT_PUBLIC_S3_BUCKET_NAME}.s3.ap-south-1.amazonaws.com/${file_key}`;
+  return url;
+}
